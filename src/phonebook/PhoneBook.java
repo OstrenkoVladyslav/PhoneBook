8e9dac8a2d853512ef05de2ca39da99d64d0aab4
Vladislav <Vladislav@10.0.1.6>
2016-01-08 16:46:59 +0200

    Null pointer exception

--- src/phonebook/PhoneBook.java
+++ src/phonebook/PhoneBook.java
@@ -7,57 +7,72 @@ import java.awt.event.*;
 
 public class PhoneBook extends Frame {
 
-    public java.awt.List data_list;
+    static java.awt.List data_list;
+    static HashMap phonebook = new HashMap();
+//    PhoneBook frame;
+//    Panel panel1, panel2, panel3;
+//    static TextField number_field, surname_field;
+//    Label number_label, surname_label;
     
-    public PhoneBook (String title) {
+    public PhoneBook (String title ) {
         super(title);
+        this.phonebook = new HashMap();
+//        this.frame = new PhoneBook(title);
+//        this.panel1 = new Panel();
+//        this.panel2 = new Panel();
+//        this.panel3 = new Panel();
+//        this.number_label = new Label("Number: ");
+//        this.surname_label = new Label("Surname: ");
+        
         try {
-            BorderLayout manager = new BorderLayout();
-            Label number_label = new Label("Number: ");
-            Label surname_label = new Label ("Surname:");
-            setLayout(manager);
-            setSize(500, 300);
-            setLocationRelativeTo(null);
-            setResizable(false);
-
-            // Создаем панель с полем "Number"
-            Panel panel = new Panel();
-            panel.add(number_label);
-            TextField number_field = (TextField) panel.add(new TextField(15));
-
-            // Создаем надпись и поле "Surname"
-            panel.add(surname_label);
-            TextField surname_field = new TextField();
-            surname_field = (TextField) panel.add(new TextField(15));
-
-            // Создаем кнопку "Add"
-            Component add_button = panel.add(new Button("Add"));
-            add (panel, BorderLayout.NORTH);
-
-            // Создаем новую панель и добавляем туда список
-            panel = new Panel();
-            this.data_list = new java.awt.List(10);
-            panel.add(data_list);
-            add(panel, BorderLayout.CENTER);
-
-
-            Button button_ok = new Button("Ok");
-            Button button_add = new Button("Add record");
-            addWindowListener(new MyWindowAdapter());
+//            BorderLayout manager = new BorderLayout();
+//            setLayout(manager);
+//            setSize(500, 300);
+//            setLocationRelativeTo(null);
+//            setResizable(false);
+//
+//            // Создаем панель с полем "Number"
+//            panel1.add(number_label);
+//            TextField number_field = (TextField) panel1.add(new TextField(15));
+//
+//            // Создаем надпись и поле "Surname"
+//            panel2.add(surname_label);
+//            TextField surname_field = (TextField) panel2.add(new TextField(15));
+//
+//            // Создаем кнопку "Add"
+//            Button add_button = (Button) panel3.add(new Button("Add"));
+//            add (panel3, BorderLayout.NORTH);
+//
+//            // Создаем новую панель и добавляем туда список
+//            Panel panel4 = new Panel();
+//            this.data_list = new java.awt.List(10);
+//            panel4.add(data_list);
+//            add(panel4, BorderLayout.CENTER);
+//
+//            Button button_ok = new Button("Ok");
+//            Button button_add = new Button("Add record");
+//
+//            //Создадим слушателей
+//            
+//
+//            addWindowListener(new MyWindowAdapter());
         }
         catch(Exception e) {
             e.printStackTrace();
         }
     }
      
-    private static void refresh_list(Map phonebook, java.awt.List data_list){
-        Set<Map.Entry<String, String>> set = phonebook.entrySet();
-        data_list.clear();
-        for (Map.Entry<String, String> me : set){
-            data_list.add(me.getKey()+"  "+me.getValue());
-        }
-
-    }
+//    static void refresh_list(){
+//        Set<Map.Entry<String, String>> set = phonebook.entrySet();
+//        //data_list.clear();
+//        for (Map.Entry<String, String> me : set){
+//            data_list.add(me.getKey()+"  "+me.getValue());
+//        }
+//    }
+    
+//    void add_record(){
+//        phonebook.put(number_field.getText(),surname_field.getText());
+//    }
     
     class MyWindowAdapter extends WindowAdapter {
         @Override
@@ -65,12 +80,22 @@ public class PhoneBook extends Frame {
             System.exit(0);
         }
     }
+//    
+//    class AddRecordListener implements ActionListener{
+//        public void actionPerformed(ActionEvent e) {
+//            //phonebook.put(number_field.getText(),);
+////            add_record();
+////            refresh_list();
+//        }
+//        
+//    }
+    
     /**
      *
      * @param args
      */
     public static void main(String[] args) {
-        Map phonebook = new HashMap();
+        //HashMap phonebook = new HashMap();
         phonebook.put("0503231014","Ostrenko");
         phonebook.put("0505775705","Petrov");
         phonebook.put("0505575007","Sidorov");
@@ -81,29 +106,38 @@ public class PhoneBook extends Frame {
         Set<Map.Entry<String, String>> set = phonebook.entrySet();
 
         // Display the set.
-        System.out.println("List of all pairs:");
-        for(Map.Entry<String, String> me : set) {
-            System.out.print(me.getKey() + ": ");
-            System.out.println(me.getValue());
-        }
-        System.out.println("--------------------------------------");
+//        System.out.println("List of all pairs:");
+//        for(Map.Entry<String, String> me : set) {
+//            System.out.print(me.getKey() + ": ");
+//            System.out.println(me.getValue());
+//        }
+//        System.out.println("--------------------------------------");
         // Finding surname by phone number
-        System.out.println("Owner of 0503231014 number is "+phonebook.get("0503231014"));
-        System.out.println("--------------------------------------");
+//        System.out.println("Owner of 0503231014 number is "+phonebook.get("0503231014"));
+//        System.out.println("--------------------------------------");
 
         // Finding phone numbers by surname
-        String search = "Sidorov";
-        System.out.println("Searching for "+search+"...");
-        for (Map.Entry<String, String> me : set){
-            if (me.getValue().equals(search))
-                System.out.println(me.getKey());
-        }
+//        String search = "Sidorov";
+//        System.out.println("Searching for "+search+"...");
+//        for (Map.Entry<String, String> me : set){
+//            if (me.getValue()==(search))
+//                System.out.println(me.getKey());
+//        }
+//        System.out.println("--------------------------------------");
+//        System.out.println("Starting windowed program...");
 
         // Создаем объект окна
-        PhoneBook frame = new PhoneBook("Phone book");
-        refresh_list(phonebook, frame.data_list);
-        frame.setVisible(true);
+//        PhoneBook frame = null;
         
+        //refresh_list();
+        
+        //Set<Map.Entry<String, String>> set = phonebook.entrySet();
+        //data_list.clear();
+        for (Map.Entry<String, String> me : set){
+            data_list.add(me.getKey()+"  "+me.getValue());
+        }
+        
+        //frame.setVisible(true);
         
     }
 }
